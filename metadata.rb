1e964f6d005647b1d91ab1ca1b6e1ec634dcd857
Kasim Tuman <kasim@runa.com>
2013-08-21 15:06:54 -0700

    bumped minor version up to 0.3.5

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "ops@runa.com"
 license          "Apache 2.0"
 description      "Installs/Configures leiningen"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.4"
+version          "0.3.5"
