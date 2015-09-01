537dfd5f8ca4ef34dc85a86826a03212147ed758
Justin Alan Ryan <juryan@ziprealty.com>
2015-09-01 12:05:26 -0700

    define name attr, required by Berkshelf/Ridley.

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
 maintainer       "Max Prokopiev"
 maintainer_email "me@maxprokopiev.com"
+name             "leiningen"
 license          "Apache 2.0"
 description      "Installs/Configures stable leiningen"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
